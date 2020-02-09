a2ce2b50b14338396d53fd14e5d5a87e6b12988b
iZeaoGamer <jameshayward14@icloud.com>
2020-02-09 17:16:47 +0000

    Update Reward.php

--- src/david/miningrewards/item/Reward.php
+++ src/david/miningrewards/item/Reward.php
@@ -25,10 +25,10 @@ class Reward extends Item {
      * @return bool
      */
     public function onClickAir(Player $player, Vector3 $directionVector): bool {
-        $itemEntity = $player->getLevel()->dropItem($player->add(0, 3, 0), $this, $directionVector->multiply(0.5), 1000);
+   //     $itemEntity = $player->getLevel()->dropItem($player->add(0, 3, 0), $this, $directionVector->multiply(0.5), 1000);
         $player->sendMessage(Loader::getPrefix() . TextFormat::GREEN . "Opening reward...!");
         $player->getInventory()->setItemInHand($this->pop());
-        Loader::getInstance()->getScheduler()->scheduleRepeatingTask(new TickTask($player, $itemEntity, Loader::getInstance()->getAnimationTickRate()), 5);
+       Loader::getInstance()->getScheduler()->scheduleRepeatingTask(new TickTask($player, $itemEntity, Loader::getInstance()->getAnimationTickRate()), 5);
         return true;
     }
 }
